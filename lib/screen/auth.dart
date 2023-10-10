829166b718fb37b7117ef5eb66a6879d9a89eaa7
Prithvi Rao Ratti <prithvi_rao_ratti@Prithvis-MacBook-Air.local>
2023-10-10 15:55:59 +0530

    Firebase commit

---
+++ lib/screen/auth.dart
@@ -0,0 +1,136 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+final firebase = FirebaseAuth.instance;
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+  final _formKey = GlobalKey<FormState>();
+  var _emailAddress;
+  var _password;
+
+  void _onSave() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    _formKey.currentState!.save();
+
+    if (_isLogin) {
+    } else {
+      try {
+        final result = await firebase.createUserWithEmailAndPassword(
+          email: _emailAddress,
+          password: _password,
+        );
+        print(result);
+      } on FirebaseAuthException catch (error) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ?? 'Authentication failed.'),
+          ),
+        );
+      }
+    }
+    //Success
+    print(_emailAddress);
+    print(_password);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                    top: 30, left: 20, bottom: 20, right: 20),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text('Email Address'),
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter valid email address.';
+                                }
+                                return null;
+                              },
+                              onSaved: (newValue) {
+                                _emailAddress = newValue;
+                              },
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text('Password'),
+                              ),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value == null || value.trim().length < 6) {
+                                  return 'Password must be minimum 6 characters.';
+                                }
+                                return null;
+                              },
+                              onSaved: (newValue) {
+                                _password = newValue;
+                              },
+                            ),
+                            const SizedBox(height: 18),
+                            ElevatedButton(
+                              onPressed: _onSave,
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer),
+                              child: Text(_isLogin ? 'Login' : 'Sign Up'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(_isLogin
+                                  ? 'Create an account'
+                                  : 'Already have an account / login'),
+                            )
+                          ],
+                        )),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
